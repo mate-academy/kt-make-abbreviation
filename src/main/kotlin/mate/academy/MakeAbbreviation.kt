2424b4c04f787f7658f5822d6609d64560c1dd86
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2024-02-25 18:13:49 +0200

    Added task impl

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,9 @@
 package mate.academy
 
+private const val SPLITTER = " "
+
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    return source.split(SPLITTER)
+        .map{it -> it[0].uppercase()}
+        .joinToString(separator = "")
 }
