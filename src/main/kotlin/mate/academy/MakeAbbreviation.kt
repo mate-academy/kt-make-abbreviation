ddfe25bd25f736a5bb43728be407b2e463e61d69
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2024-03-06 00:33:57 +0200

    done

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,8 @@
 package mate.academy
 
+const val FIRST = 0
+
 fun makeAbbr(source: String) : String {
     // implement this function
-    return ""
+    return source.trim().split(" ").joinToString("") { it[FIRST].toString().uppercase() }
 }
