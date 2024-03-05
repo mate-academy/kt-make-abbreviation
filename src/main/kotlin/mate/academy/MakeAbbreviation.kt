5e99ca32454004584990106dd6cc20d22ec273d0
VolodymyrDavidov <volodymyrdavidov1@gmail.com>
2024-03-05 14:46:50 +0200

    done

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,12 @@
 package mate.academy
 
-fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+fun makeAbbr(source: String): String {
+    val words = source.split(" ")
+    var abbreviation = ""
+
+    for (word in words) {
+        abbreviation += word[0].uppercase()
+    }
+
+    return abbreviation
 }
