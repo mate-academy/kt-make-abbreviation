822bfbdf24d70f5bddb2edfc9a0ef135db35fee3
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2024-03-15 10:40:55 +0200

    first version

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,11 @@
 package mate.academy
 
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    var answer: String = source[0].toString()
+    for (i in source.indices) {
+        if (source[i] == ' ') {
+            answer += source[i + 1]
+        }
+    }
+    return answer.uppercase()
 }
