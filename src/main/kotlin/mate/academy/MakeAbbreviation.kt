57e7aee24d57bb015bd18f77fff377151a5b2c28
khorkovenko <khorkovenko>
2024-03-29 10:30:58 +0200

    done

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,18 @@
 package mate.academy
 
-fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+import java.util.*
+
+fun makeAbbr(source: String): String {
+    val words = source.split(" ")
+    val abbreviation = StringBuilder()
+
+    for (word in words) {
+        if (word.length > 1) {
+            abbreviation.append(word[0].uppercaseChar())
+        } else if (word.isNotEmpty()) {
+            abbreviation.append(word.uppercase(Locale.getDefault()))
+        }
+    }
+
+    return abbreviation.toString()
 }
