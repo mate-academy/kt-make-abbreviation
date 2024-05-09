814a2a44adcb450e3d0455c6e641a652236f0b3f
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-05-09 07:37:18 +0300

    MakeAbbreviation.kt

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,8 @@
 package mate.academy
 
-fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+fun makeAbbr(source: String): String {
+    return source.split(" ")
+                 .filter { it.isNotEmpty() }
+                 .map { it[0].uppercaseChar() }
+                 .joinToString("")
 }
