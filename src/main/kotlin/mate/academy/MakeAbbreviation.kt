38c16259920956a5b22aa53de947fb2c11c7342e
AntonBabychP1T <antonbabi13@gmail.com>
2024-05-25 12:16:05 +0300

    the task is completed

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,10 @@
 package mate.academy
 
+fun main(args: Array<String>) {
+    println(makeAbbr("simplified molecular input line entry specification"))
+}
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    var result : String = source[0].uppercase()
+    source.forEachIndexed { index, c -> if(c == ' ') result += source[index + 1].uppercase()  }
+    return result
 }
