3fbc3e9f3105e7a0c9cc00b1cc7929b480ce859a
Roman Globa <globaroman@gmail.com>
2024-06-09 20:24:47 +0300

    The solution of the task

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,11 @@
 package mate.academy
 
+const val DELIMITER = " "
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    val split = source.split(DELIMITER)
+    val sb = StringBuilder()
+    for (s in split) {
+        sb.append(s[0].uppercase())
+    }
+    return sb.toString()
 }
