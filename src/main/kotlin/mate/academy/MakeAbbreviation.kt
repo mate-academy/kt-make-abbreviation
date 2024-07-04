9369548b612f9981607620bb46287685526a7c5d
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-07-04 14:23:55 +0300

    added hw solution

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,12 @@
 package mate.academy
 
-fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+fun makeAbbr(source: String): String {
+    return source.split(" ")
+        .map {
+            it.first().uppercase()
+        }
+        .joinToString {
+            it
+        }
+        .replace(", ","")
 }
