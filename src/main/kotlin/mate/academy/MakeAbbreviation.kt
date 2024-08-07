bcffc0492aed1e5c055338be910830b40bb31cfd
John <pandastars535@gmail.com>
2024-08-07 03:51:05 +0300

    solution

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,9 @@
 package mate.academy
 
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+
+    var split = source.split(" ")
+    val result = split.asSequence().map { it.substring(0, 1).uppercase() }.toList().joinToString("")
+
+    return result
 }
