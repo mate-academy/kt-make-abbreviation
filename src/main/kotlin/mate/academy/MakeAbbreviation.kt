edfecc10cce0bb0901f2f7f4c03e57810d264fbe
Anatolii Nazarov <anatoly_n@ukr.net>
2024-11-25 14:03:41 +0200

    For check

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,11 @@
 package mate.academy
 
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    val words: List<String> = source.split(" ")
+    val builder = StringBuilder()
+
+    for (word in words) {
+        builder.append(word[0].toString().uppercase())
+    }
+    return builder.toString()
 }
