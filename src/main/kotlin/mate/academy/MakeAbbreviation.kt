6972d329df6f60f2169a109b2ca824c00d49e965
oleksii.b <oleksii.b@dev.tagsoft.co>
2025-01-16 15:56:37 +0200

    removed magic number

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,9 +1,11 @@
 package mate.academy
 
+private const val FIRST_CHAR = 0
+
 fun makeAbbr(source: String): String {
     val resultList = mutableListOf<String>()
     source.split(" ").forEach { name ->
-        val sourceChar = name.get(0).uppercase()
+        val sourceChar = name.get(FIRST_CHAR).uppercase()
         resultList.add(sourceChar)
     }
     return resultList.joinToString("")
