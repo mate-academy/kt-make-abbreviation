d63257182e10a562fa23da56c5ecd6f842214ef6
oleksii.b <oleksii.b@dev.tagsoft.co>
2025-01-16 15:55:52 +0200

    resolved task

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,10 @@
 package mate.academy
 
-fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+fun makeAbbr(source: String): String {
+    val resultList = mutableListOf<String>()
+    source.split(" ").forEach { name ->
+        val sourceChar = name.get(0).uppercase()
+        resultList.add(sourceChar)
+    }
+    return resultList.joinToString("")
 }
