76d618e7da55c4c67b308b750e27cf49bf3f7d07
OD <sdenisenko055@gmail.com>
2025-03-05 11:32:58 +0200

    result

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,15 @@
 package mate.academy
 
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    var result : String = ""
+    if (source.isBlank()) {
+        return result
+    }
+    result = result.plus(source[0]).uppercase()
+    for(i in 1 until source.length) {
+        if (source[i] == ' ') {
+            result = result.plus(source[i + 1].uppercaseChar())
+        }
+    }
+    return result
 }
