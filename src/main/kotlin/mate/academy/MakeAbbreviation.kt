eafe755c8dfce7c1d565a6a52785425a06d5af53
aksoli666 <olexsandradorofeieva@gmail.com>
2025-03-18 15:04:22 +0100

    1.0 Done Abbreviation

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,11 @@
 package mate.academy
 
 fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+    var result: String = source.first().toString()
+    source.split(" ").forEach {
+        if (it.isNotEmpty()) {
+            result += it.first()
+        }
+    }
+    return result
 }
